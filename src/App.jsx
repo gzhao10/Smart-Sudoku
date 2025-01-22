56c28a7f8a1eedcb0b9835db9ea0532930eea10c
gzhao10 <zgavin73@gmail.com>
2025-01-22 17:07:56 -0500

    initial commits

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Grid from './components/Grid'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Grid/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Grid.jsx
@@ -0,0 +1,13 @@
+const Cell = (value) => {
+    return (
+        <div>Sudoku</div>
+    )
+}
+
+const Grid = () => {
+    return (
+        <Cell/>
+    )
+}
+
+export default Grid
